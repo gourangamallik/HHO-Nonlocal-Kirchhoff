dee60142f0e80e64459843e1010e8aef8696cb7c
Gouranga Mallik <105980027+gourangamallik@users.noreply.github.com>
2026-07-05 16:35:24 +0530

    Update Newton_solver.m

--- Newton_solver.m
+++ Newton_solver.m
@@ -8,7 +8,8 @@ function [uTF,d,nIter] = Newton_solver(idofs,A_global,AS_global, b_global, Prev_
 % ----------------------------------------------------------------------
 N=length(b_global); dofs=[idofs, N+1];
 uTF=sparse(N,1);beta=[Prev_uTF;Prev_d];
-Iter_Err=1; nIter=0; tic;TOL=10^(-10); Newton_Err=[];relax_param=1;
+Iter_Err=1; nIter=0; tic;TOL=10^(-10); Newton_Err=[];
+fprintf("Convergence of the Newton iterations:\n");
 while(Iter_Err>TOL && nIter<50)
     nIter=nIter+1;
     d = Prev_uTF'*(A_global*Prev_uTF); Md=1+d;
@@ -18,13 +19,12 @@ while(Iter_Err>TOL && nIter<50)
     Jacobian=[JA   Jb;
               Jc'  -1];
 
-    beta(dofs) = beta(dofs) - relax_param * Jacobian(dofs,dofs)\F(dofs);
+    beta(dofs) = beta(dofs) - Jacobian(dofs,dofs)\F(dofs);
     uTF = beta(1:N); d = beta(N+1);
 
     Iter_Err=sqrt((uTF-Prev_uTF)'*(AS_global*(uTF-Prev_uTF)))/(uTF'*(AS_global*uTF));
     Prev_uTF=uTF; Prev_d=d;
     Newton_Err=[Newton_Err, Iter_Err];
+    fprintf('%d  %.4e \n',nIter,Iter_Err);
 end
-%Newton_OC = compute_Newton_Convergence(Newton_Err,relax_param); %compute the rate of convergence of the Newton Iterations
 end
-
